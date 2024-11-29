885e23eee06986af584eacacd87914ef32a5479d
pedrojaokkj <jpdr3551@gmail.com>
2024-11-28 23:29:37 -0300

    Aula 2 - Botao

--- src/index.js
+++ src/index.js
@@ -1,36 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-
-class ClockClass extends React.Component{
+class Toggle extends React.Component{
 
   constructor(props){
     super(props)
-    this.state = {date : new Date()}
+    this.state = {isToggleOn : false}
+    //this.handleClick = this.handleClick.bind(this)
   }
 
-  componentDidMount(){
-    this.timerId = setInterval(() => {
-      this.setState({date : new Date()})
-    }, 1000)
+  handleClick(){
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    })  )
   }
-
-  componentWillUnmount(){
-    clearInterval(this.timerId)
-  }
-
 
   render(){
-    return (<div><h1> HORA : {this.state.date.toLocaleTimeString()} </h1></div>  )
+    return <button onClick={() => this.handleClick()}>{this.state.isToggleOn ? 'ON' : 'OFF'}</button>
   }
-
 }
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ClockClass date = {new Date()} />
+  <Toggle/>
 );
 
 
