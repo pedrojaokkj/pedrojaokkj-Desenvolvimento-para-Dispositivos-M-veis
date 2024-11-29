75fb2eb8e5cc70993ec4562856f75764d2ccd646
pedrojaokkj <jpdr3551@gmail.com>
2024-11-28 23:16:00 -0300

    Aula 2 - Relogio

--- src/index.js
+++ src/index.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Welcome(props){
-  return <h1>Hello {props.name}</h1>
+
+class ClockClass extends React.Component{
+
+  constructor(props){
+    super(props)
+    this.state = {date : new Date()}
+  }
+
+  componentDidMount(){
+    this.timerId = setInterval(() => {
+      this.setState({date : new Date()})
+    }, 1000)
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timerId)
+  }
+
+
+  render(){
+    return (<div><h1> HORA : {this.state.date.toLocaleTimeString()} </h1></div>  )
+  }
+
 }
 
-function App(){
-  return (<div> 
-    <Welcome name = "Paulo"/>
-    <Welcome name = "Maria"/>
-  </div>)
-}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App/>
+  <ClockClass date = {new Date()} />
 );
+
+
+
+
 
