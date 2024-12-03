d770658b3d8140533234a2b81554ac5415630186
pedrojaokkj <jpdr3551@gmail.com>
2024-12-03 18:29:16 -0300

    Aula 3 - Controled Component Select

--- src/index.js
+++ src/index.js
@@ -1,23 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-
+import ReactDOM from 'react-dom';
 
 class Form extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state = { value: 'React!' }
+    this.state = { value: 'branco' }
     this.handleChance = this.handleChance.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleChance(event) {
     this.setState({ value: event.target.value })
   }
 
+  handleSubmit(event) {
+    alert('Um novo evento foi enviado: ' + this.state.value)
+    event.preventDefault()
+  }
+
   render() {
     return (
-      <form>
-        <input onChange={this.handleChance} value={this.state.value} />
+      <form onSubmit={this.handleSubmit}>
+        <select value={this.state.value} onChange={this.handleChance}>
+          <option value="laranja">Laranja</option>
+          <option value="branco">Branco</option>
+          <option value="verde">Verde</option>
+          <option value="amarelo">Amarelo</option>
+        </select>
+        <input type="submit" value="Enviar!" />
       </form>
     )
   }
